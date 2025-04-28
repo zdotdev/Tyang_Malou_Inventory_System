06938e9844c56aec0a2a2d91b8b1627e6dff79ba
zdotdev <www.ragaszedrick25@gmail.com>
2025-04-28 12:03:48 +0800

    Added basic user restriction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,19 +1,18 @@
 <?php
-
 namespace App\Controller;
-
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController{
     #[Route("/home", name: "app_home")]
-
-    public function index(ProductRepository $productRepository): Response{
-
+    public function index(Request $request, ProductRepository $productRepository): Response{
+        $username = $request->query->get('main');
         return $this->render('home/index.html.twig',[
             'products' => $productRepository->findAll(),
+            'u' => $username
         ]);
     }
 }
