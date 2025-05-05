7c8d7ce6c27b385a5ea66134cad57553097ff026
zdotdev <www.ragaszedrick25@gmail.com>
2025-05-05 20:21:55 +0800

    new migration version

--- migrations/Version20250505121945.php
+++ migrations/Version20250505121945.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250427133646 extends AbstractMigration
+final class Version20250505121945 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,10 +21,10 @@ final class Version20250427133646 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, employee_name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, employee_name VARCHAR(255) NOT NULL, employee_username VARCHAR(255) NOT NULL, employee_password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
-            CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, product_name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, supplier INT NOT NULL, cost_price DOUBLE PRECISION NOT NULL, selling_price DOUBLE PRECISION NOT NULL, unit_stock INT NOT NULL, delivery_date VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+            CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, product_name VARCHAR(255) NOT NULL, category VARCHAR(255) NOT NULL, supplier VARCHAR(255) NOT NULL, cost_price DOUBLE PRECISION NOT NULL, selling_price DOUBLE PRECISION NOT NULL, unit_stock INT NOT NULL, delivery_date DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
         SQL);
         $this->addSql(<<<'SQL'
             CREATE TABLE sales (id INT AUTO_INCREMENT NOT NULL, total_ammount DOUBLE PRECISION NOT NULL, employee_name VARCHAR(255) NOT NULL, date_created DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
